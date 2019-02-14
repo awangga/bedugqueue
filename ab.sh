b22a915b805fbb402652bb05f1bc1c2405f1650e
Rolly Maulana Awangga <rolly@awang.ga>
2019-02-14 13:48:51 +0700

    ok

--- ab.sh
+++ ab.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
+echo "--------------------------"
 echo "1 req conncurrent 1"
-ab -n1 -c1 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
+echo "--------------------------"
+ab -n1 -c1 "http://spacemap.lapan.go.id/spacemap-aceh/"
+echo "--------------------------"
+echo "10 req conncurrent 5"
+echo "--------------------------"
+sleep 10
+ab -n10 -c5 "http://spacemap.lapan.go.id/spacemap-aceh/"
+echo "--------------------------"
 echo "100 req conncurrent 10"
+echo "--------------------------"
 sleep 10
-ab -n10 -c5 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
-echo "100 req conncurrent 50"
+ab -n100 -c10 "http://spacemap.lapan.go.id/spacemap-aceh/"
+echo "--------------------------"
+echo "500 req conncurrent 20"
+echo "--------------------------"
 sleep 10
-ab -n100 -c50 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
-echo "500 req conncurrent 200"
+ab -n500 -c20 "http://spacemap.lapan.go.id/spacemap-aceh/"
+echo "--------------------------"
+echo "1000 req conncurrent 30"
+echo "--------------------------"
 sleep 10
-ab -n1000 -c500 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
-echo "700 req conncurrent 300"
+ab -n1000 -c30 "http://spacemap.lapan.go.id/spacemap-aceh/"
+echo "--------------------------"
+echo "1000 req conncurrent 50"
+echo "--------------------------"
 sleep 10
-ab -n10000 -c5000 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
-echo "1000 req conncurrent 500"
+ab -n1000 -c50 "http://spacemap.lapan.go.id/spacemap-aceh/"
+echo "--------------------------"
+echo "1000 req conncurrent 100"
+echo "--------------------------"
 sleep 10
-ab -n100000 -c50000 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
-echo "10000 req conncurrent 5000"
-sleep 10
-ab -n1000000 -c500000 "http://192.168.1.3:8080/s.py?rcpt=089610707901&msg=haikenomordariab"
+ab -n1000 -c100 "http://spacemap.lapan.go.id/spacemap-aceh/"
